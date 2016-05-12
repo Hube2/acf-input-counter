59d23a006961b3a76b975fc8a724d31ffa8f8f4b
John Huebner <johnhuebner@site-seeker.com>
2016-05-12 19:20:42 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# acf-input-counter
+# ACF Input Counter
+
+Supports ACF 5
+
 Adds a counter to all text and textarea fields with character limits
+
+This is a simple plugin that will add a counter below all ACF text and text area fields to show how many
+characters have been added and what the limit is. The display will look something like this:
+
+```
+chars: 25 of 55
+```
+
+This plugin will not work in ACF 4 for 2 reasons:
+* The acf/render_field hook is not supported by ACF 4
+* The JavaScript used in this plugin is specific to ACF 5
+
+This plugin also serves as a simple example of how to extend the functionality of fields in ACF 5.
---
+++ acf-input-counter.js
@@ -0,0 +1,43 @@
+
+(function($) {
+	acf.fields.text_counter = acf.field.extend({
+		type: 'text',
+		
+		events: {
+			'input input': 'change_count',
+		},
+		
+		change_count: function(e){
+			var $max = e.$el.attr('maxlength');
+			if (typeof($max) == 'undefined') {
+				return;
+			}
+			var $value = e.$el.val();
+			var $length = $value.length;
+			e.$el.closest('.acf-input').find('.count').text($length);
+		},
+		
+	});
+})(jQuery);
+
+
+(function($) {
+	acf.fields.textarea_counter = acf.field.extend({
+		type: 'textarea',
+		
+		events: {
+			'input textarea': 'change_count',
+		},
+		
+		change_count: function(e){
+			var $max = e.$el.attr('maxlength');
+			if (typeof($max) == 'undefined') {
+				return;
+			}
+			var $value = e.$el.val();
+			var $length = $value.length;
+			e.$el.closest('.acf-input').find('.count').text($length);
+		},
+		
+	});
+})(jQuery);
