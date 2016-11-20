7bcc9b42b782f9d5de02733f8bbed5a3c75bd10c
Berry Plasman <info@berryplasman.com>
2016-11-20 02:21:08 +0100

    added condition display

--- acf-input-counter.php
+++ acf-input-counter.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 	/*
 		Plugin Name: ACF Input Counter
@@ -21,21 +21,21 @@
 		private $version = '1.2.0';
 
 		public function __construct() {
-			add_action('acf/render_field/type=text', array($this, 'render_field'), 20, 1);
-			add_action('acf/render_field/type=textarea', array($this, 'render_field'), 20, 1);
-			add_action('acf/input/admin_enqueue_scripts', array($this, 'scripts'));
-			add_filter('jh_plugins_list', array($this, 'meta_box_data'));
+			add_action('acf/render_field/type=text', 		array($this, 'render_field'), 20, 1);
+			add_action('acf/render_field/type=textarea', 	array($this, 'render_field'), 20, 1);
+			add_action('acf/input/admin_enqueue_scripts', 	array($this, 'scripts'));
+			add_filter('jh_plugins_list', 					array($this, 'meta_box_data'));
 		} // end public function __construct
-			
+
 			function meta_box_data($plugins=array()) {
-				
+
 				$plugins[] = array(
-					'title' => 'ACF Input Counter',
-					'screens' => array('acf-field-group', 'edit-acf-field-group'),
-					'doc' => 'https://github.com/Hube2/acf-input-counter'
+					'title' 	=> 'ACF Input Counter',
+					'screens' 	=> array('acf-field-group', 'edit-acf-field-group'),
+					'doc' 		=> 'https://github.com/Hube2/acf-input-counter'
 				);
 				return $plugins;
-				
+
 			} // end function meta_box
 
 		private function run() {
@@ -54,10 +54,10 @@
 				return;
 			}
 			// wp_enqueue_script
-			$handle = 'acf-input-counter';
-			$src = plugin_dir_url(__FILE__).'acf-input-counter.js';
-			$deps = array('acf-input');
-			$ver = $this->version;
+			$handle    = 'acf-input-counter';
+			$src       = plugin_dir_url(__FILE__).'acf-input-counter.js';
+			$deps      = array('acf-input');
+			$ver       = $this->version;
 			$in_footer = false;
 			wp_enqueue_script($handle, $src, $deps, $ver, $in_footer);
 			wp_enqueue_style('acf-counter', plugins_url( 'acf-counter.css' , __FILE__ ));
@@ -73,20 +73,20 @@
 			}
 			$len = strlen($field['value']);
 			$max = $field['maxlength'];
-			
-			$classes = apply_filters('acf-input-counter/classes', array());
-			$ids = apply_filters('acf-input-counter/ids', array());
-			
+
+			$classes 	= apply_filters('acf-input-counter/classes', array());
+			$ids 		= apply_filters('acf-input-counter/ids', array());
+
 			$insert = true;
 			if (count($classes) || count($ids)) {
 				$insert = false;
-				
+
 				$exist = array();
 				if ($field['wrapper']['class']) {
 					$exist = explode(' ', $field['wrapper']['class']);
 				}
 				$insert = $this->check($classes, $exist);
-				
+
 				if (!$insert && $field['wrapper']['id']) {
 					$exist = array();
 					if ($field['wrapper']['id']) {
@@ -95,7 +95,7 @@
 					$insert = $this->check($ids, $exist);
 				}
 			} // end if filter classes or ids
-				
+
 			if (!$insert) {
 				return;
 			}
@@ -103,15 +103,18 @@
 			$display = apply_filters('acf-input-counter/display', $display);
 			$display = str_replace('%%len%%', '<span class="count">'.$len.'</span>', $display);
 			$display = str_replace('%%max%%', $max, $display);
+
+			if (!$field['wrapper']['class'] == 'hide' ) {
 			?>
 				<span class="char-count">
-					<?php 
+					<?php
 						echo $display;
 					?>
 				</span>
 			<?php
+			}
 		} // end public function render_field
-		
+
 		private function check($allow, $exist) {
 			// if there is anything in $allow
 			// see if any of those values are in $exist
@@ -123,50 +126,50 @@
 		} // end private function check
 
 	} // end class acf_input_counter
-	
+
 	if (!function_exists('jh_plugins_list_meta_box')) {
 		function jh_plugins_list_meta_box() {
 			if (apply_filters('remove_hube2_nag', false)) {
 				return;
 			}
 			$plugins = apply_filters('jh_plugins_list', array());
-				
-			$id = 'plugins-by-john-huebner';
-			$title = '<a style="text-decoration: none; font-size: 1em;" href="https://github.com/Hube2" target="_blank">Plugins by John Huebner</a>';
-			$callback = 'show_blunt_plugins_list_meta_box';
-			$screens = array();
+
+			$id 		= 'plugins-by-john-huebner';
+			$title 		= '<a style="text-decoration: none; font-size: 1em;" href="https://github.com/Hube2" target="_blank">Plugins by John Huebner</a>';
+			$callback 	= 'show_blunt_plugins_list_meta_box';
+			$screens 	= array();
 			foreach ($plugins as $plugin) {
 				$screens = array_merge($screens, $plugin['screens']);
 			}
-			$context = 'side';
-			$priority = 'low';
+			$context 	= 'side';
+			$priority 	= 'low';
 			add_meta_box($id, $title, $callback, $screens, $context, $priority);
-			
-			
+
+
 		} // end function jh_plugins_list_meta_box
 		add_action('add_meta_boxes', 'jh_plugins_list_meta_box');
-			
+
 		function show_blunt_plugins_list_meta_box() {
 			$plugins = apply_filters('jh_plugins_list', array());
 			?>
 				<p style="margin-bottom: 0;">Thank you for using my plugins</p>
 				<ul style="margin-top: 0; margin-left: 1em;">
-					<?php 
+					<?php
 						foreach ($plugins as $plugin) {
 							?>
 								<li style="list-style-type: disc; list-style-position:">
-									<?php 
+									<?php
 										echo $plugin['title'];
 										if ($plugin['doc']) {
-											?> <a href="<?php echo $plugin['doc']; ?>" target="_blank">Documentation</a><?php 
+											?> <a href="<?php echo $plugin['doc']; ?>" target="_blank">Documentation</a><?php
 										}
 									?>
 								</li>
-							<?php 
+							<?php
 						}
 					?>
 				</ul>
-				<p><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=hube02%40earthlink%2enet&lc=US&item_name=Donation%20for%20WP%20Plugins%20I%20Use&no_note=0&cn=Add%20special%20instructions%20to%20the%20seller%3a&no_shipping=1&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted" target="_blank">Please consider making a small donation.</a></p><?php 
+				<p><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=hube02%40earthlink%2enet&lc=US&item_name=Donation%20for%20WP%20Plugins%20I%20Use&no_note=0&cn=Add%20special%20instructions%20to%20the%20seller%3a&no_shipping=1&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted" target="_blank">Please consider making a small donation.</a></p><?php
 		}
 	} // end if !function_exists
 
