83e3df0cb64a82eaa0e9f5c3cd16aaf0aed702c9
Beee <info@berryplasman.com>
2018-06-03 03:03:20 +0200

    Update version

--- acf-input-counter.php
+++ acf-input-counter.php
@@ -4,7 +4,7 @@
 		Plugin Name: ACF Input Counter
 		Plugin URI: https://github.com/Hube2/acf-input-counter/
 		Description: Show character count for limited text and textarea fields
-		Version: 1.4.1
+		Version: 1.5
 		Author: John A. Huebner II
 		Author URI: https://github.com/Hube2/
 		Text-domain: acf-counter
@@ -20,7 +20,7 @@
 
 	class acf_input_counter {
 
-		private $version = '1.4.1';
+		private $version = '1.5';
 
 		public function __construct() {
 			add_action('plugins_loaded', 					array($this, 'acf_counter_load_plugin_textdomain'));
